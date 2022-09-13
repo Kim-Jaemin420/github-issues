edff037bed59946f4edabd62a6e9ed2795edbf55
Kim-Jaemin420 <jany240@naver.com>
2022-09-14 01:17:07 +0900

    feat: 공통 레이아웃에 header 추가

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { Header } from '@/components';
 
 function Layout() {
   return (
     <>
-      공통 레이아웃
+      <Header />
       {<Outlet />}
     </>
   );
