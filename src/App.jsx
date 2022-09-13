cb078d36c4c3bca5350fd31e66b00af1c80ff91b
Kim-Jaemin420 <jany240@naver.com>
2022-09-14 00:41:40 +0900

    feat: 라우터 추가
    - issue list 페이지와 issue detail 페이지의 라우터 연결
      동일한 header를 사용할 예정으로 공통 레이아웃을 사용
      할 수 있게 Layout 컴포넌트도 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { IssueDetail, IssueList } from './pages';
+import { Layout } from './components';
+import { ISSUE_DETAIL_PAGE, ISSUE_LIST_PAGE } from './consts';
+
 function App() {
-  return <div className="App">github issues</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path={ISSUE_LIST_PAGE} element={<IssueList />} />
+            <Route path={ISSUE_DETAIL_PAGE} element={<IssueDetail />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
